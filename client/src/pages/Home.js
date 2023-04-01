656fb0170ee23f2fb19277d7cbe3c72f3baa0c87
calumkuhn <ckuhn@smu.edu>
2023-03-31 20:17:07 -0500

    added home logic

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,14 +7,52 @@ const Home = () => {
     const [messages, setMessages] = useState([]); // Fetch messages from API
     const [currentRoomId, setCurrentRoomId] = useState(null);
 
-    const handleJoin = (roomId) => {
-        // Implement room joining logic
-        setCurrentRoomId(roomId);
+    const handleJoin = async (roomId) => {
+        try {
+            const roomResponse = await fetch(`/api/chatrooms/${roomId}`);
+            if (!roomResponse.ok) {
+                throw new Error('Failed to fetch chat room');
+            }
+
+            const roomData = await roomResponse.json();
+
+            const messagesResponse = await fetch(`/api/chatrooms/${roomId}/messages`);
+            if (!messagesResponse.ok) {
+                throw new Error('Failed to fetch messages');
+            }
+
+            const messagesData = await messagesResponse.json();
+
+            setChatRooms([...chatRooms, roomData]);
+            setMessages(messagesData);
+            setCurrentRoomId(roomId);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    const handleSendMessage = (message) => {
-        // Implement message sending logic
+
+    const handleSendMessage = async (message) => {
+        try {
+            const response = await fetch(`/api/chatrooms/${currentRoomId}/messages`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ message }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to send message');
+            }
+
+            const newMessage = await response.json();
+            setMessages([...messages, newMessage]);
+        } catch (error) {
+            console.error(error);
+        }
     };
+
 
     return (
         <div>
